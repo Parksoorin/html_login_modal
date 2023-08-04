fbfcb647c415340b63efa02e296faaf8d4c1ce53
soorin <rin4330@naver.com>
2023-08-04 16:17:25 +0900

    first commit

---
+++ login.js
@@ -0,0 +1,8 @@
+const modal = document.getElementById('myModal');
+
+function handleOpen () {
+    modal.style.display = 'block'; /* 모달 창을 보이도록 설정합니다. */
+};
+function handleClose() {
+    modal.style.display = 'none'; /* 모달 창을 숨깁니다. */
+};
